2954a2ccbf31f2049d0b2f4be8a89813b1657b4c
rajjubajra <rajjubajra@gmail.com>
2021-08-13 19:20:39 +0100

    build file test

--- tw2-01-music/src/App.js
+++ tw2-01-music/src/App.js
@@ -1,12 +1,11 @@
 import Home from './Pages/Home';
-import {Provider} from 'react-redux';
-import {store} from './redux/store';
+
 
 function App() {
   return (
-    <Provider store={store}>
-      <Home />
-    </Provider>
+    <div>
+        <Home />
+    </div>
   );
 }
 
