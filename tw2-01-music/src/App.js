1ea28b6f83dfd24cf69534d8fc1002213c75519e
rajjubajra <rajjubajra@gmail.com>
2021-08-14 23:15:07 +0100

    Router  updates

--- tw2-01-music/src/App.js
+++ tw2-01-music/src/App.js
@@ -31,13 +31,13 @@ function App() {
         <BrowserRouter>
           <MainNav />
           <Switch>
-            <Route path={`${baseurl}`} component={Homepage} />
+            <Route exact path={`${baseurl}`} component={Homepage} />
             <Route path={`${baseurl}/tour`} component={Tour} />
             <Route path={`${baseurl}/news`} component={News} />
             <Route path={`${baseurl}/about`} component={About} />
             <Route path={`${baseurl}/music`} component={Music} />
             <Route path={`${baseurl}/gallery`} component={Gallery} />
-            <Route path={`${baseurl}/contact`} component={Contact} />
+            <Route path={`${baseurl}/form/contact`} component={Contact} />
           </Switch>
           
         </BrowserRouter>
