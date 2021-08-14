11ef9809de8940de504068dca914e7af8fcd70f6
rajjubajra <rajjubajra@gmail.com>
2021-08-15 00:37:46 +0100

    fetching homepage data

--- tw2-01-music/src/redux/actions.js
+++ tw2-01-music/src/redux/actions.js
@@ -34,7 +34,7 @@ export const action = () => {
                     : NavRemote;
 
   /** HOME PAGE */
-  const homeUrl = `${baseurl.URL}/jsonapi/media/image?include=field_media_image`;
+  const homeUrl = `${baseurl.URL}/jsonapi/media/image/6a41bf3a-8adb-4a6a-a998-024ae6de1f20?include=field_media_image`;
   const HOME = homeUrl;
   
 
