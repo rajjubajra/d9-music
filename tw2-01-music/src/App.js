8cb3d1289f5e4e6ecb272681640893f444b7335b
rajjubajra <rajjubajra@gmail.com>
2021-08-14 22:51:46 +0100

    tailwind css installed

--- tw2-01-music/src/App.js
+++ tw2-01-music/src/App.js
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-import './App.css';
 import Homepage from './page/Homepage';
 import {useDispatch} from 'react-redux';
 import {action} from './redux/actions';
