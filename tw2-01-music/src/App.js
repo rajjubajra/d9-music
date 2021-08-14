3db9c4c667f765de1415ee8a71e60dc91295490a
rajjubajra <rajjubajra@gmail.com>
2021-08-14 14:48:56 +0100

    main nav

--- tw2-01-music/src/App.js
+++ tw2-01-music/src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import {Provider} from 'react-redux';
-import {store}from './redux/store';
 import Homepage from './page/Homepage';
 
 
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <Provider store={store}>
+        
             <Homepage />
-        </Provider>
+      
       </header>
     </div>
   );
---
+++ tw2-01-music/src/components/MainNav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MainNav() {
+
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MainNav
