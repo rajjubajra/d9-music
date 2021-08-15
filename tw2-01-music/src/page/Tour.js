a9f327d523189dc0bda0846f2be0eeb54910ad71
rajjubajra <rajjubajra@gmail.com>
2021-08-15 03:05:06 +0100

    homepage view

--- tw2-01-music/src/page/Tour.js
+++ tw2-01-music/src/page/Tour.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {useSelector} from 'react-redux';
+
 
 function Tour() {
+
+  const data = useSelector(state => state.reducerTour.tour_data.data);
+  const length = useSelector(state => state.reducerTour.tour_dataLength);
+
+  console.log(data, "length", length);
+
+
   return (
     <div>
       <h1>Tour Page</h1>
