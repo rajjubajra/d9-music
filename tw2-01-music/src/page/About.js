c7dd6ee7e8c709ac2626741cc23dafbd0a69b6e4
rajjubajra <rajjubajra@gmail.com>
2021-08-15 15:49:44 +0100

    about  page updates

--- tw2-01-music/src/page/About.js
+++ tw2-01-music/src/page/About.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {actionAbout} from '../redux/actions';
 
@@ -12,10 +12,32 @@ function About() {
   },[dispatch])
 
   const state = useSelector(state => state.reducerAbout.about_data.data);
+  const length = useSelector(state => state.reducerAbout.about_dataLength);
   const inc_data = useSelector(state => state.reducerAbout.about_data.included);
 
   console.log('about data', state, "about lng", inc_data);
 
+  const [arr, setArr] = useState([]);
+
+  useEffect(()=>{
+
+    const data = [];
+    length > 0 &&
+    state.map(item =>{
+      const {attributes:{title, field_about_page}} = item;
+      inc_data.map(inc => {
+        if(inc.type === 'file--file'){
+          const {attributes:{uri}} = inc;
+          data.push({title: title, body: field_about_page.processed, image: uri.url})
+        }
+        return setArr(data);
+      })
+    })
+    
+  },[inc_data, length, state])
+
+  console.log("Array about data",arr);
+  
   return (
     <div>
       <h1>About Page</h1>
