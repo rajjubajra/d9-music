ae3970606d2dda41a741261f5e084026b0cc6edb
rajjubajra <rajjubajra@gmail.com>
2021-08-15 03:38:01 +0100

    tour page data fetching

--- tw2-01-music/src/page/Tour.js
+++ tw2-01-music/src/page/Tour.js
@@ -1,7 +1,7 @@
 import React,{useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {actionTour} from '../redux/actions'
-
+import {Link} from 'react-router-dom';
 
 function Tour() {
 
@@ -19,7 +19,23 @@ function Tour() {
 
   return (
     <div>
-      <h1>Tour Page</h1>
+    {
+      length > 0 &&
+        data.map(item => {
+          const {attributes:{title, field_event_details, field_event_location, field_buy_ticket}} = item;
+        return <div>
+          <h1 className="text-2xl">{title}</h1>
+          <div>
+            <h2>Details:</h2>
+            <div dangerouslySetInnerHTML={{__html: field_event_details.value}} />
+            <h2>Location:</h2>
+            <div dangerouslySetInnerHTML={{__html: field_event_location.value}} />
+            <h2>Ticket</h2>
+            <div><Link to={field_buy_ticket.uri}>{field_buy_ticket.title}</Link></div>
+          </div>
+          </div>
+      })
+    }
     </div>
   )
 }
