0f896ee59c5a96a0515ba160cd9d394931d02e57
rajjubajra <rajjubajra@gmail.com>
2021-08-15 12:17:09 +0100

    actions js updated

--- tw2-01-music/src/page/News.js
+++ tw2-01-music/src/page/News.js
@@ -1,7 +1,6 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {actionNews} from '../redux/actions';
-
 
 
 
@@ -9,12 +8,38 @@ function News() {
 
   const dispatch = useDispatch();
   const data = useSelector(state => state.reducerNews.news_data.data);
+  const inc_data = useSelector(state => state.reducerNews.news_data.included);
   const length = useSelector(state => state.reducerNews.news_dataLength);
+
+  const [arr, setArr] = useState([]);
 
   console.log("news",data,"news length", length);
   useEffect(()=>{
     dispatch(actionNews());
-  },[dispatch])
+
+    const newdata = [];
+    console.log("newdata", newdata);
+    length > 0 && data.map(item => {
+      const {attributes:{title, field_news_body, field_news_date}} = item;
+      
+      inc_data.map(inc => {
+
+        if(inc.type === 'file--file'){
+          const {attributes:{uri}} = inc;    
+          return newdata.push({
+            title: title, 
+            body: field_news_body.value, 
+            data: field_news_date, 
+            image: uri.url});
+        }
+      })
+    })
+
+    setArr(newdata);
+    
+  },[data, dispatch, length, inc_data])
+
+  console.log("data arr news", arr);
   
   return (
     <div>
