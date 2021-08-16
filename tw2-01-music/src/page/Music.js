b3815a628f594038e991fcc6d58fa0d9cece11e8
rajjubajra <rajjubajra@gmail.com>
2021-08-16 14:55:35 +0100

    music data fetching

--- tw2-01-music/src/page/Music.js
+++ tw2-01-music/src/page/Music.js
@@ -5,12 +5,27 @@ import {actionMusic} from '../redux/actions';
 function Music() {
   
   const dispatch = useDispatch();
+  const state = useSelector(state => state.reducerMusic.music_data.data);
+  const inc_data = useSelector(state => state.reducerMusic.music_data.included);
+  const length = useSelector(state => state.reducerMusic.music_dataLength);
   
   useEffect(()=>{
     dispatch(actionMusic());
   },[dispatch])
 
-  
+  const [arr, setArr] = useState([]);
+
+  useEffect(()=>{
+    const data = [];
+    console.log(data);
+    length > 0 &&
+    state.map(item => {
+        const {attributes:{title,field_music_body}, relationships:{field_music_audio:{data}}} = item;
+        data.push({title: title, body: field_music_body.processed, data: data});
+    })
+
+  },[length, state])
+
 
   return (
     <div>
