65c5ebdf976863e72704c4dfb50c70f63ac4a1fe
rajjubajra <rajjubajra@gmail.com>
2021-08-18 08:51:14 +0100

    media data fetching

--- tw2-01-music/src/page/Music.js
+++ tw2-01-music/src/page/Music.js
@@ -6,10 +6,6 @@ function Music() {
   
   const dispatch = useDispatch();
   const state = useSelector(state => state.reducerMusic.music_data.data);
-  /**
-   * //const mediaAudio = useSelector(state.reducerMediaAudio.media_audio_data );
-   */
-  
   const inc_data = useSelector(state => state.reducerMusic.music_data.included);
   const length = useSelector(state => state.reducerMusic.music_dataLength);
   
