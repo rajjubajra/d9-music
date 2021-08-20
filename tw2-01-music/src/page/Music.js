80d11603bee47dd72e049e9c4abe88c7cecde6e9
rajjubajra <rajjubajra@gmail.com>
2021-08-20 16:58:00 +0100

    media data fetching

--- tw2-01-music/src/page/Music.js
+++ tw2-01-music/src/page/Music.js
@@ -10,39 +10,49 @@ function Music() {
   const mediaAudioInc = useSelector(state => state.reducerMediaAudio.media_audio_data.included);
   const mediaAudioLength = useSelector(state => state.reducerMediaAudio.media_audio_dataLength);
 
-  const state = useSelector(state => state.reducerMusic.music_data.data);
-  const inc_data = useSelector(state => state.reducerMusic.music_data.included);
-  const length = useSelector(state => state.reducerMusic.music_dataLength);
+  const musicData = useSelector(state => state.reducerMusic.music_data.data);
+  const musicDataInc = useSelector(state => state.reducerMusic.music_data.included);
+  const musicDataLength = useSelector(state => state.reducerMusic.music_dataLength);
   
   
 
   const [newArr, setNewArr] = useState([]); 
   const [audioData, setAudiData] = useState([]);
   
-
+  /** RUN action  */
   useEffect(()=>{
     dispatch(actionMusic());
     dispatch(actionMediaAudio());
   },[dispatch])
 
+  //console.log(mediaAudio, mediaAudioInc,mediaAudioLength);
+  console.log(musicData, musicDataInc, musicDataLength);
+
+  /** AudioMedia "data" array  and "included" array merged */
+  useEffect(()=>{
+    const audio = [];
+    console.log("audio arr: ", audio);
+
+    mediaAudioLength > 0 &&
+    mediaAudio.map(item => {
+      const {id} = item;
+      mediaAudioInc.map(inc => {
+        const {attributes:{filemime, filename,filesize,uri}} = inc;
+        /** stop re-looping push if "id" exists */
+        const hasId = audio.some(key => key.id === id);
+        return !hasId && audio.push({id: id, filetype: filemime, filename: filename, filesize:filesize, uri: uri })
+      })
+    })
+    
+    setAudiData(audio);
+
+  },[mediaAudio, mediaAudioInc, mediaAudioLength])
   
-  console.log(mediaAudio, mediaAudioInc,mediaAudioLength);
+  console.log("Audio data +: ",audioData);  
 
 
-  console.log(state, inc_data, length);
 
-  // const Arr =  lng1 > 0 && lng2 &&
-  //   arr1.map((item) => {
-  //       //const {attributes:{title, field_music_body}} = item;
-  //       const {relationships:{field_music_audio:{data}}} = item;
-  //       data.map((dt, index) => {
-  //         if(dt.id === arr2[index]){
-  //           return Object.assign({},item, arr2[index]);
-  //         } 
-  //       })
-  //   })
-  // }
-
+  /** FUNCTION: merge Array with same "id"  */
   function mergeArrayObjects(arr1,arr2){
     
     arr1.map((item,i) => {
@@ -54,27 +64,21 @@ function Music() {
   }
 
 
+  /** AudioMedia and MusicData */
   useEffect(()=>{
-    const audio = [];
-    console.log("audio arr: ", audio);
-
-    mediaAudioLength > 0 &&
-    mediaAudio.map(item => {
-      const {id} = item;
-      mediaAudioInc.map(inc => {
-        const {attributes:{filemime, filename,filesize,uri}} = inc;
-        const hasId = audio.some(key => key.id === id);
-        return !hasId && audio.push({id: id, filetype: filemime, filename: filename, filesize:filesize, uri: uri })
+    const arr = [];
+    musicDataLength > 0 && audioData.length > 0 &&
+    musicData.map((item)=>{
+      const {attributes:{title, field_music_body}} = item;
+      const {relationships:{file_music_audio:{data}}} = item;
+      arr.push({
+        title: title, 
+        body: field_music_body,
+        data: mergeArrayObjects(data, audioData)
       })
     })
-    
-    setAudiData(audio);
 
-  },[mediaAudio, mediaAudioInc, mediaAudioLength])
-  
-
-
-  console.log("Audio data +: ",audioData);  
+  },[audioData, musicData, musicDataLength])
 
   return (
     <div>
