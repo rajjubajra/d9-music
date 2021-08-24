04fe936bd0bc7d35bca708f0f8119c7aef0bc1db
rajjubajra <rajjubajra@gmail.com>
2021-08-24 19:47:51 +0100

    music.js updates

--- tw2-01-music/src/page/Music/AudioList.js
+++ tw2-01-music/src/page/Music/AudioList.js
@@ -3,20 +3,18 @@ import React from 'react'
 
 function AudioList(props) {
 
-  console.log("test - data", props.data);
+  //console.log("test - data", props.data);
 
   return (
-    
-    <div>- Audio List -</div>
-    // data.map(item =>{
-    //   const {title, uri:{url}} = item;
-    //   return <li>
-    //     <h3>{title}</h3>
-    //     <div>{url}</div>
-    //   </li>
-    // })
-    
+    props.data.map(item =>{
+      const {title, uri:{url}} = item;
+      return <li>
+        <h3>{title}</h3>
+        <div>{url}</div>
+      </li>
+    })
   )
+  
 }
 
 export default AudioList
