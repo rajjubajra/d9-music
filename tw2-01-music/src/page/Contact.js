3643daffee8c6d7a25a287b957e7ef7e6c41b419
rajjubajra <rajjubajra@gmail.com>
2021-08-26 10:27:56 +0100

    working on contact.js file

--- tw2-01-music/src/page/Contact.js
+++ tw2-01-music/src/page/Contact.js
@@ -5,14 +5,14 @@ function Contact() {
     <div>
       <h1>Contact Page</h1>
       <div className="w-full">
-        <form class="w-full md:3/6 border border-gray-50 p-4 m-5 flex justify-center align-middle flex-col">
+        <form class="w-full md:w-3/6 border border-gray-300 p-4 m-5 flex justify-center align-middle flex-col">
         <div>
-          <input className="border border-gray-100 p-2" type="text" id="name"  name="name"  placeholder="Name" />
-          <input className="border border-gray-100 p-2"  type="text" id="email" name="email" placeholder="Email" />
+          <input className="w-full md:w-1/2 border md:border-l md:border-r md:border-t md:border-b-0 border-gray-500 p-2" type="text" id="name"  name="name"  placeholder="Name" />
+          <input className="w-full md:w-1/2 border md:border-l md:border-r md:border-t-0 md:border-b-0 border-gray-500 p-2"  type="text" id="email" name="email" placeholder="Email" />
         </div>
         <div>
-          <textarea className="w-auto md:w-3/6 border border-gray-100 p-2 h-60"  name="message" id="message"  placeholder="Message"></textarea>
-        </div>  
+          <textarea className="w-full border border-gray-500 p-2 h-60"  name="message" id="message"  placeholder="Message"></textarea>
+        </div>
         <div>
           <input  className="border border-gray-500 p-4"  type="submit" id="submit" name="submit" value="Submit" />
         </div>          
