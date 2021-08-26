e1df391ab390b1eaced6f640cfb9095e9ab273e6
rajjubajra <rajjubajra@gmail.com>
2021-08-26 10:07:15 +0100

    working on contact.js file

--- tw2-01-music/src/page/Contact.js
+++ tw2-01-music/src/page/Contact.js
@@ -5,13 +5,13 @@ function Contact() {
     <div>
       <h1>Contact Page</h1>
       <div className="w-full">
-        <form class="w-full md:3/6 border border-gray-50 p-4 m-5 flex flex-col">
+        <form class="w-full md:3/6 border border-gray-50 p-4 m-5 flex justify-center align-middle flex-col">
         <div>
           <input className="border border-gray-100 p-2" type="text" id="name"  name="name"  placeholder="Name" />
           <input className="border border-gray-100 p-2"  type="text" id="email" name="email" placeholder="Email" />
         </div>
         <div>
-          <textarea className="border border-gray-100 p-2"  name="message" id="message"  placeholder="Message"></textarea>
+          <textarea className="w-full border border-gray-100 p-2 h-60"  name="message" id="message"  placeholder="Message"></textarea>
         </div>  
         <div>
           <input  className="border border-gray-500 p-4"  type="submit" id="submit" name="submit" value="Submit" />
