4faddac55c722458733637419c51fe1243aacdb0
rajjubajra <rajjubajra@gmail.com>
2021-08-26 10:38:30 +0100

    working on contact.js file

--- tw2-01-music/src/page/Contact.js
+++ tw2-01-music/src/page/Contact.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
+      <h1 className="text-3xl text-center m-5">Contact Form</h1>
       <div className="w-full flex justify-center align-middle">
-        <form class="w-full md:w-3/6 border border-gray-300 p-4 m-5 flex flex-col">
+        <form class="w-full md:w-3/6 p-4 m-5 flex flex-col">
         <div>
           <input className="w-full md:w-1/2 border md:border-l md:border-r md:border-t md:border-b-0 border-gray-500 p-2 outline-none" type="text" id="name"  name="name"  placeholder="Name" />
           <input className="w-full md:w-1/2 border md:border-l-0 md:border-r md:border-t md:border-b-0 border-gray-500 p-2 outline-none"  type="text" id="email" name="email" placeholder="Email" />
