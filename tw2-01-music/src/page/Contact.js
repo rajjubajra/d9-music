579909cac17e98adf02e5df7d59546f4ac8ec156
rajjubajra <rajjubajra@gmail.com>
2021-08-27 09:08:42 +0100

    working on contact.js file

--- tw2-01-music/src/page/Contact.js
+++ tw2-01-music/src/page/Contact.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import {baseurl} from '../config/config';
+import axios from 'axios';
 
 
 
@@ -17,16 +19,39 @@ function Contact() {
   const handleSubmit = (e) =>{
     e.preventDefault();
     console.log(name, email, message);
+    const headers = {
+      headers: {
+      'Accept': 'application/vnd.api+json'
+      }
+    }
+
+    const webform_rest_url = `${baseurl.URL}/webform_rest/submit`;
+
+    axios({
+      method: 'post',
+      url: webform_rest_url,
+      headers: headers,
+      data: {
+        "webform_id": "contact",
+        "name": name,
+        "email": email,
+        "message": message
+      }
+    })
+    .then(res=> console.log(res))
+    .catch(err => console.log(err))
+
   }
 
   return (
     <div>
       <h1 className="text-3xl text-center m-5">Contact Form</h1>
       <div className="w-full flex justify-center align-middle">
-        <form onSubmit={handleSubmit}  class="w-full md:w-3/6 p-4 m-5 flex flex-col">
+        <form onSubmit={handleSubmit}  method="post" class="w-full md:w-3/6 p-4 m-5 flex flex-col">
         <div>
           <input 
           className="w-full md:w-1/2 border md:border-l md:border-r md:border-t md:border-b-0 border-gray-500 p-2 outline-none" 
+          required
           type="text" 
           id="name"  
           name="name"  
@@ -35,7 +60,8 @@ function Contact() {
           onChange={(e)=>setName(e.target.value)}
           />
           <input 
-          className="w-full md:w-1/2 border md:border-l-0 md:border-r md:border-t md:border-b-0 border-gray-500 p-2 outline-none"  
+          className="w-full md:w-1/2 border md:border-l-0 md:border-r md:border-t md:border-b-0 border-gray-500 p-2 outline-none"
+          required  
           type="text" 
           id="email" 
           name="email" 
@@ -47,6 +73,7 @@ function Contact() {
         <div>
           <textarea 
           className="w-full border border-gray-500 p-2 h-60 outline-none"  
+          required
           name="message" 
           id="message"  
           placeholder="Message"
