ad2ab5eeac1061c6eac2b2430116aae7cbdccf5f
rajjubajra <rajjubajra@gmail.com>
2021-08-29 19:42:30 +0100

    date fetching laternative tesing on app.js file

--- tw2-01-music/src/components/ImageOnload.js
+++ tw2-01-music/src/components/ImageOnload.js
@@ -1,14 +1,20 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function ImageOnload({src, alt, cssClass}) {
 
   const [status, setStatus] = useState(false);
+  const [blur, setBlur] = useState('blur-3xl');
   console.log(status);
 
+  useEffect(()=>{
+
+    status && setBlur('blur-none');
+
+  },[status])
+
   return (
-    status &&
     <img 
-    className={cssClass}
+    className={`${blur} ${cssClass}`}
     onLoad={()=>setStatus(true)}
     onError={()=>setStatus(false)}
     src={src} 
