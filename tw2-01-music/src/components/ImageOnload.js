9362b8e8f76bcb03d2731069133d4142b7e848b1
rajjubajra <rajjubajra@gmail.com>
2021-08-29 22:24:49 +0100

    date fetching laternative tesing on app.js file

--- tw2-01-music/src/components/ImageOnload.js
+++ tw2-01-music/src/components/ImageOnload.js
@@ -3,22 +3,22 @@ import React, {useState, useEffect} from 'react'
 function ImageOnload({src, alt, cssClass, height, width}) {
 
   const [status, setStatus] = useState(false);
-  //const [blur, setBlur] = useState('hidden');
-  const [imgHeight, setImgHeight] = (height/10);
-  const [imgWidth, setImgWidth] = (width/10);
+  const [blur, setBlur] = useState('blur-3xl');
+  const [imgHeight, setImgHeight] = (parseInt(height)/10);
+  const [imgWidth, setImgWidth] = (parseInt(width)/10);
   console.log(status);
 
   useEffect(()=>{
 
-    status && setImgHeight('auto');
-    status && setImgWidth('auto');
+    status && setImgHeight('auto') && setImgWidth('auto') && setBlur('blur-none')
+
   },[status])
 
 
   return (
     <img 
     width={imgWidth} height={imgHeight}
-    className={`${cssClass}`}
+    className={`${blur} ${cssClass}`}
     onLoad={()=>setStatus(true)}
     onError={()=>setStatus(false)}
     src={src} 
