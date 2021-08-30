5965277f59fe3957d7ac0cc1cf32c71252c01172
rajjubajra <rajjubajra@gmail.com>
2021-08-30 12:39:02 +0100

    MainMenu tailwind style added

--- tw2-01-music/src/components/MainMenu/Logo.js
+++ tw2-01-music/src/components/MainMenu/Logo.js
@@ -2,8 +2,8 @@ function Logo() {
   return (
     <div>
       <img 
-        width='3rem'
-        height='3rem'
+        width='100px'
+        height='auto'
         src="./yellow-website-square150x150.png" 
         alt="logo" />
     </div> 
