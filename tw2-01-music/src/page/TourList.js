3330039ab8d5fae8524ecea6ad9c804c94317323
rajjubajra <rajjubajra@gmail.com>
2021-09-02 16:02:55 +0100

    tour list style added

--- tw2-01-music/src/page/TourList.js
+++ tw2-01-music/src/page/TourList.js
@@ -1,3 +1,4 @@
+import { data } from 'autoprefixer';
 import React from 'react';
 import {Link} from 'react-router-dom';
 
@@ -8,16 +9,20 @@ function TourList({tourdata}) {
         tourdata.length > 0 &&
         tourdata.map( item => {
           const {attributes:{title, field_event_date, field_event_details, field_event_location:{processed}, field_buy_ticket}} = item;
+          const date = new Date(field_event_date);
+
           return <div key={item.id} 
           className="grid grid-rows-3 gap-1
                     md:grid-cols-7 md:grid-rows-1 mb-8">
             <div className="border border-gray-300 p-2 col-span-2 
-                            md:col-span-1">{field_event_date}</div>
+                            md:col-span-1">
+                            {date.getDate()}.{date.getMonth()}.{date.getFullYear()}
+                            </div>
             <div className="border border-gray-300 col-span-2
                             md:col-span-4 p-2">
               <div>
                 <h2>{title}</h2>
-                <div>{processed}</div>
+                <div><div dangerouslySetInnerHTML={{__html: processed }} /></div>
               </div>
             </div>
             <div className="border border-gray-300 
