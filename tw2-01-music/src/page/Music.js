feb396c1f948e97031ecca73c0b6c5ac5b218d72
rajjubajra <rajjubajra@gmail.com>
2021-09-04 16:49:07 +0100

    working on music.js file

--- tw2-01-music/src/page/Music.js
+++ tw2-01-music/src/page/Music.js
@@ -77,15 +77,15 @@ function Music(){
   function getUri(id, arr){
     console.log("mediaId uri", id)
     arr.map( item => {
-      console.log(item.id===id ? item.uri : item.id);
-      return item.id === id && item.uri
+      //console.log(item.id===id ? item.uri : item.id);
+      return item.id === id ? item.uri : item.id
     });
   }
   function getFileType(id, arr){
     console.log("meidiaId filetype",typeof(id));
     arr.map(item => {
-      console.log(item.id===id ? item.filetype : typeof(item.id));
-      return item.id === id && item.filetype
+      //console.log(item.id===id ? item.filetype : typeof(item.id));
+      return item.id === id ? item.filetype : item.id
     } );
   }
 
