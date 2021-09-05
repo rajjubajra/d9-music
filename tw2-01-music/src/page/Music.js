34e856e302b326747b48f7ef050879d3c172a3eb
rajjubajra <rajjubajra@gmail.com>
2021-09-05 14:10:52 +0100

    working on music.js file

--- tw2-01-music/src/page/Music.js
+++ tw2-01-music/src/page/Music.js
@@ -87,7 +87,7 @@ function Music(){
 
   function getUrlForMusicAudio(arr1, arr2){
       const newArr = [];
-      arr2.length > 0 &&
+      arr2.length > 0 && arr1.length > 0 &&
       arr2.map( item => {
         return arr1.some( el => el.id === item.id ) &&
         newArr.push({
