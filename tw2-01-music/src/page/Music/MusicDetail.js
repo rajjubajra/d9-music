afe43d89f29ebe52cef6c5e75c58f49cdeb3e03f
rajjubajra <rajjubajra@gmail.com>
2021-09-06 09:33:53 +0100

    working on music.js file

--- tw2-01-music/src/page/Music/MusicDetail.js
+++ tw2-01-music/src/page/Music/MusicDetail.js
@@ -7,11 +7,22 @@ function MusicDetail({singleData}) {
   
   console.log("single data",singleData);
   console.log("passsing",typeof(singleData));
+
+  console.log("If undefined", singleData  !== undefined && singleData.title)
   
 
   return (
     <div>
-      <h1>Music Page</h1>
+      {
+        singleData !== undefined &&
+        <div className="m-5 p-3">
+            <h2 className="my-2">{singleData.title}</h2>
+            <div dangerouslySetInnerHTML={{__html: singleData.body}} />
+            <ul className="m-5 border p-4">
+              
+            </ul>            
+      </div>
+      }
     </div>
   )
 }
