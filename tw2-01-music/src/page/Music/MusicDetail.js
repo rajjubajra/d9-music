62c35e13aacec2d49a1ddd85f4ad5ddcbfab1298
rajjubajra <rajjubajra@gmail.com>
2021-09-06 09:23:21 +0100

    working on music.js file

--- tw2-01-music/src/page/Music/MusicDetail.js
+++ tw2-01-music/src/page/Music/MusicDetail.js
@@ -5,24 +5,13 @@ import ImageOnload from '../../components/ImageOnload';
 function MusicDetail({singleData}) {
   
   
-  console.log("passsing",singleData);
+  console.log("single data",singleData);
+  console.log("passsing",typeof(singleData));
   
 
   return (
     <div>
       <h1>Music Page</h1>
-      {
-        
-        <div className="m-5 p-3">
-            <h2 className="my-2">{singleData.title}</h2>
-            
-            <div dangerouslySetInnerHTML={{__html: singleData.body}} />
-          
-            <ul className="m-5 border p-4">
-              
-            </ul>            
-          </div>
-      }
     </div>
   )
 }
