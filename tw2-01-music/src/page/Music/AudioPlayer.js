6921802392ad35dbdbb6c616b6ce89e7f6be2ef2
rajjubajra <rajjubajra@gmail.com>
2021-09-06 18:00:45 +0100

    working on music.js file

--- tw2-01-music/src/page/Music/AudioPlayer.js
+++ tw2-01-music/src/page/Music/AudioPlayer.js
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import {BiPlay, BiPause} from 'react-icons/bi';
 
 const useAudio = url => {
   const [audio] = useState(new Audio(url));
   const [playing, setPlaying] = useState(false);
+  const [currentUpdate] = useRef();
 
   const toggle = () => setPlaying(!playing);
 
   useEffect(() => {
       playing ? audio.play() : audio.pause();
-      console.log("time update", audio.currentTime);
+      
     },[audio, playing]);
 
   useEffect(() => {
     audio.addEventListener('ended', () => setPlaying(false));
     return () => {
       audio.removeEventListener('ended', () => setPlaying(false));
-      
     };
-    
   }, [audio]);
+
+  useEffect(()=>{
+    
+  })
 
   console.log("duration?",audio.duration, audio);
   
@@ -29,11 +33,25 @@ const useAudio = url => {
 
 
 const AudioPlayer = ({ url }) => {
+
+  const ProgressBar = ({ progressPercentage }) => {
+    return (
+        <div className='h-1 w-full bg-gray-300'>
+            <div
+                style={{ width: `${progressPercentage}%`}}
+                className={`h-full ${
+                    progressPercentage < 70 ? 'bg-red-600' : 'bg-green-600'}`}>
+            </div>
+        </div>
+    );
+ };
+
   const [playing, toggle] = useAudio(url);
 
   return (
     <div>
-      <button onClick={toggle}>{playing ? "Pause" : "Play"}</button>
+      {ProgressBar(80)}
+      <button onClick={toggle}>{playing ? <BiPause /> : <BiPlay />}</button>
     </div>
   );
 };
