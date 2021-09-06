9c91505603a9f24880635b988c7caa8c92a3ed15
rajjubajra <rajjubajra@gmail.com>
2021-09-06 09:44:23 +0100

    working on music.js file

--- tw2-01-music/src/page/Music/MusicDetail.js
+++ tw2-01-music/src/page/Music/MusicDetail.js
@@ -17,10 +17,23 @@ function MusicDetail({singleData}) {
         singleData !== undefined &&
         <div className="m-5 p-3">
             <h2 className="my-2">{singleData.title}</h2>
-            <div dangerouslySetInnerHTML={{__html: singleData.body}} />
             <ul className="m-5 border p-4">
-              
-            </ul>            
+              <ImageOnload 
+                src={singleData.image[0].url}
+                alt="Cover"
+              />
+            </ul>   
+            <div dangerouslySetInnerHTML={{__html: singleData.body}} />
+            <div>
+              <h3>Play songs</h3>
+              <ul>
+              {
+                singleData.audio.map((item) => {
+                  return <li>{item.name} - {item.url.url}</li>
+                })
+              }
+              </ul>
+            </div>
       </div>
       }
     </div>
