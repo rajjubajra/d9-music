162c27e7e256508a8e5c28231e1712db1d53243e
rajjubajra <rajjubajra@gmail.com>
2021-09-09 15:17:54 +0100

    music.js file

--- tw2-01-music/src/page/SubscriptionForm.js
+++ tw2-01-music/src/page/SubscriptionForm.js
@@ -62,7 +62,7 @@ function SubscriptionForm() {
   }
 
   return (
-    <div>
+    <div className="w-full p-10">
       <h1 className="text-3xl text-center m-5">
         {form_fetched && form.attributes.title}
       </h1>
@@ -72,7 +72,7 @@ function SubscriptionForm() {
         {submitForm && form_fetched && form.attributes.settings.confirmation_message }
       </div>
       <div className="w-full flex justify-center align-middle">
-        <form onSubmit={handleSubmit}  method="post" class="w-full md:w-3/6 p-4 m-5 flex flex-col">
+        <form onSubmit={handleSubmit}  method="post" class="w-full p-4 m-5 flex flex-col">
         <div>
           <input 
           className="w-full outline-none p-2 border-gray-500 
