d529195a2677133dffd3faf390f18cafaa1bae10
rajjubajra <rajjubajra@gmail.com>
2021-09-09 13:51:36 +0100

    music.js file

--- tw2-01-music/src/page/Music/MusicDetail.js
+++ tw2-01-music/src/page/Music/MusicDetail.js
@@ -1,6 +1,11 @@
 import React,{useEffect} from 'react';
 import ImageOnload from '../../components/ImageOnload';
 import Iframe from 'react-iframe';
+import {RiAmazonFill} from 'react-icons/ri';
+import {FaSpotify, FaGooglePlay, FaYoutube} from 'react-icons/fa';
+import {SiApplemusic} from 'react-icons/si';
+import BtnBack from '../../components/Buttons/BtnBack';
+//import {AiOutlinePlayCircle} from 'react-icons/ai';
 
 
 
@@ -22,8 +27,59 @@ function MusicDetail({singleData}) {
 
   return (
     <div>
-      {
-        singleData !== undefined &&
+      { singleData !== undefined &&
+      <div className="grid grid-cols-12 gap-8 max-w-screen-xl m-auto p-10 font-extralight tracking-wide">
+
+      <div className="col-span-11 text-right">share</div>
+      <div className="text-center"><BtnBack /></div>
+      
+      <div className="col-span-12 md:col-span-6">
+        <div>
+          <div>
+            <h3 className="text-3xl my-5">{singleData.title}</h3>
+          </div>
+          <div>
+            <ImageOnload src={singleData.image[0].url} />
+          </div>
+          <div className="my-6">
+            <div dangerouslySetInnerHTML={{__html: singleData.body}} />
+          </div>
+        </div>
+      </div>
+      
+      <div className="col-span-12 md:col-span-6">
+
+        <div className="px-10">
+          <h3 className="py-4">Shop</h3>
+          <div className="grid grid-cols-2 grid-rows-2 gap-4">
+            <div><RiAmazonFill /></div>
+            <div><FaSpotify /></div>
+            <div><FaGooglePlay /></div>
+            <div><SiApplemusic /></div>
+          </div>
+        </div>
+
+        <div className="px-10 py-8">
+          <h3 className="py-4">Listen</h3>
+          <div className="grid grid-cols-6 grid-rows-1">
+            <div><FaYoutube /></div>
+            <div><FaSpotify /></div>
+            <div><SiApplemusic /></div>
+          </div>
+        </div>
+
+
+        <div className="px-10 py-8">
+          <h3 className="py-4">Play</h3>
+          <Iframe  
+            url="https://open.spotify.com/embed/playlist/37i9dQZF1DX3PFzdbtx1Us" width="100%" 
+            height="380" 
+            id="myId" />
+        </div>
+
+      </div>
+    </div>
+        /* singleData !== undefined &&
         <div className="m-5 p-3">
             <h2 className="my-2">{singleData.title}</h2>
             <ul className="m-5 border p-4">
@@ -40,11 +96,8 @@ function MusicDetail({singleData}) {
               height="380" 
               id="myId" 
               />
-            
-              
-              
             </div>
-      </div>
+      </div> */
       }
     </div>
   )
