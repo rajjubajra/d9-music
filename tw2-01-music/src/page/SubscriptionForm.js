3dd76c4d38b7aff41006e5fcc4cce06ed4d0d043
rajjubajra <rajjubajra@gmail.com>
2021-09-12 06:34:52 +0100

    subscription form

--- tw2-01-music/src/page/SubscriptionForm.js
+++ tw2-01-music/src/page/SubscriptionForm.js
@@ -24,7 +24,7 @@ function SubscriptionForm() {
 
   useEffect(()=>{
     
-    dispatch(actionSubscriptionForm())
+    submitForm && dispatch(actionSubscriptionForm())
 
     submitForm &&
     setName('');
