0856e3aaffb5c762955b27cc6ec65167746ccab4
rajjubajra <rajjubajra@gmail.com>
2021-09-15 13:47:55 +0100

    logo.js file update

--- tw2-01-music/src/components/MainMenu/Logo.js
+++ tw2-01-music/src/components/MainMenu/Logo.js
@@ -1,25 +1,19 @@
-import {useState, useEffect} from 'react';
+
+
 
 function Logo() {
 
-  const [loaded, setLoaded] = useState(false);
-  const [imgUrl, setimgUrl] = useState('');
 
   const url = ['./yellow-website-square150x150.png', './../yellow-website-square150x150'];
-  
-  for(let i = 0; i < url.length && !loaded ; i++) {
-    setimgUrl(url[i])
-    loaded && setLoaded(true);
-  }
+
+  console.log(url);
 
   return (
     <div>
       <img 
         width='50px'
         height='auto'
-        src={imgUrl}
-        onLoad= {() => setLoaded(true)}
-        onError= {() => setLoaded(false)}
+        src={url[0]}
         alt="logo" />
     </div> 
   )
