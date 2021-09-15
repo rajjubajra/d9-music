1e7eb6abe1893a8b697d4b0d2b3fb6625dfba07c
rajjubajra <rajjubajra@gmail.com>
2021-09-15 12:52:35 +0100

    subscription form

--- tw2-01-music/src/components/MainMenu/Logo.js
+++ tw2-01-music/src/components/MainMenu/Logo.js
@@ -1,10 +1,22 @@
+import {useState} from 'react';
+
 function Logo() {
+
+  const url_1 = './yellow-website-square150x150.png';
+  const url_2 = '../yellow-website-square150x150.png'
+
+  const [imageLoaded, setImageLoaded] = false;
+
+
+
   return (
     <div>
       <img 
         width='50px'
         height='auto'
-        src="./yellow-website-square150x150.png" 
+        src={imageLoaded ? url_1 : url_2 }
+        onLoad={()=>setImageLoaded(true)} 
+        onError={()=>setImageLoaded(false)}
         alt="logo" />
     </div> 
   )
