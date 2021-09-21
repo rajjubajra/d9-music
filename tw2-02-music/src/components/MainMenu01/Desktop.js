c8f731fd3e28cb0eaf1759717718fa2fa1e679b6
rajjubajra <rajjubajra@gmail.com>
2021-09-21 16:04:56 +0100

    tw2-02 menubar

--- tw2-02-music/src/components/MainMenu01/Desktop.js
+++ tw2-02-music/src/components/MainMenu01/Desktop.js
@@ -2,20 +2,21 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 
-function Desktop({menudata, menu}) {
+function Desktop({menudata, length,menu}) {
 
   return (
     <div className="flex justify-between">
           <div className="px-3 text-xl font-extralight">YW Music Two</div>
           <div className="flex">
           { 
-          menudata.length > 0 && 
+          length > 0 && 
           menudata.map((item)=>{
-            return menu.some(el => el === item.title) &&
+            const {attributes:{title}} = item;
+            return menu.some(el => el === title) &&
             <div>
               <Link className="px-4 py-4 m-1 font-extralight 
               tracking-wider uppercase text-sm cursor-pointer" to="#">
-              {item.title}</Link> 
+              {title}</Link> 
             </div>
             })
           }
