47a46a36553b310867222157f2dc40aaf7d64e6b
rajjubajra <rajjubajra@gmail.com>
2021-09-21 09:14:07 +0100

    tw2-02 contactForm.js

--- tw2-02-music/src/page/ContactForm.js
+++ tw2-02-music/src/page/ContactForm.js
@@ -76,9 +76,8 @@ function Contact() {
           <div className="col-span-2 md:col-span-1">
             <input 
             className="border-t border-l border-r 
-            md:border-r-0
-            border-gray-300 
-            p-1 w-full" 
+            md:border-r-0 border-gray-300 
+            outline-none p-1 w-full" 
             type="text" 
             placeholder="Name" 
             value={name}
@@ -87,8 +86,8 @@ function Contact() {
           </div>  
           <div className="col-span-2 md:col-span-1">
             <input 
-            className="border-t border-l border-r 
-            border-gray-300 p-1 w-full" 
+            className="border-t border-l border-r border-gray-300 
+            outline-none p-1 w-full" 
             type="text" 
             placeholder="Email" 
             value={email}
@@ -97,13 +96,15 @@ function Contact() {
           </div>
           <div className="h-20 col-span-2">
             <textarea 
-            className="col-span-2 border border-gray-300 p-1 w-full h-20" placeholder="Message"
+            className="col-span-2 border border-gray-300 
+            outline-none p-1 w-full h-20" placeholder="Message"
             value={message}
             onChange={(e)=>setMessage(e.target.value)}></textarea>
           </div>
           <div className="col-span-2 mt-2">
             <input 
-            className="px-4 py-1 h-10 border border-gray-400 text-gray-500 cursor-pointer" 
+            className="px-4 py-1 h-10 border border-gray-400 
+            outline-none text-gray-500 cursor-pointer" 
             type="submit" 
             value="Submit" />
           </div>  
