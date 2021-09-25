edcc5f3003475ae52a3fed3eb44b71b6aaab7c49
rajjubajra <rajjubajra@gmail.com>
2021-09-25 18:43:32 +0100

    tw2-03 tour.js

--- tw2-03-music/src/page/TourList02.js
+++ tw2-03-music/src/page/TourList02.js
@@ -27,6 +27,9 @@ function TourList02({tourdata}) {
   console.log("view details",viewDetail);
   console.log("rTour data", tourdata);
 
+
+  const month = ["","Jan","Feb","Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
   return (
     <div className="w-full max-w-screen-xl m-auto p-10 relative
     grid grid-cols-1 md:grid-cols-2 grid-flow-row gap-2">
@@ -35,14 +38,18 @@ function TourList02({tourdata}) {
       <div>
         <h2 className="text-3xl">Tour</h2>
         {
-          tourdata.length > 0 &&
+          fetched > 0 &&
           tourdata.map((item, index) => {
+              const {attributes:{title, field_event_date, field_event_details, field_event_location:{processed}, field_buy_ticket}} = item;
+              const date = new Date(field_event_date);
               return  <div key={item.id} className="grid grid-cols-12 grid-flow-row gap-2 py-5
               border-t border-gray-300">
-                <div className="col-span-12">{item.data}</div>
                 <div className="col-span-12">
-                  <h2 className="text-2xl">{item.event_title}</h2>
-                  <div>{item.venue}</div>
+                {month[date.getMonth()]} {date.getDate()}, {date.getFullYear()}
+                </div>
+                <div className="col-span-12">
+                  <h2 className="text-2xl">{title}</h2>
+                  <div>{processed}</div>
                 </div>
                 <div className="col-span-6 border border-gray-200">
                   <div className="cursor-pointer p-2 text-sm flex"
@@ -70,7 +77,7 @@ function TourList02({tourdata}) {
         
         
         <h2 className="text-3xl">
-          {fetched && tourdata[event].event_title}
+          {fetched && tourdata[event].title}
         </h2>
         <div className="grid grid-col-12 grid-flow-row gap-5">
           <div className="col-span-6">
