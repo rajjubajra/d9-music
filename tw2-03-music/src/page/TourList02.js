43a1a6c10ff6ea7382e5778acbd35f5e22d8b108
rajjubajra <rajjubajra@gmail.com>
2021-09-26 15:47:06 +0100

    tour page udpates

--- tw2-03-music/src/page/TourList02.js
+++ tw2-03-music/src/page/TourList02.js
@@ -1,52 +1,28 @@
-import React,{useEffect, useState} from 'react';
-import {GrFormNextLink, GrClose} from 'react-icons/gr';
+import {GrFormNextLink} from 'react-icons/gr';
 
 
 
-function TourList02({tourdata, monthArr, ViewDetail}) {
+function TourList02({date, title, venue, view, index}) {
 
 
   return (
-    <div className="w-full max-w-screen-xl m-auto p-10 relative grid grid-cols-1 md:grid-cols-2 grid-flow-row gap-2 font-extralight">
-
-      {/** TOUR LIST */}
-      <div>
-        <h2 className="text-3xl">Tour</h2>
-        {
-          tourdata.length > 0 &&
-          tourdata.map((item, index) => {
-              const {attributes:{title, field_event_date, field_event_details, field_event_location:{processed}, field_buy_ticket}} = item;
-          
-              return  <div key={item.id} className="grid grid-cols-12 grid-flow-row gap-2 py-5
+      <div className="grid grid-cols-12 grid-flow-row gap-2 py-5
               border-t border-gray-300">
-                <div className="col-span-12">
-                  {field_event_date}
-                </div>
-                <div className="col-span-12">
+          <div className="col-span-12">{date}</div>
+              <div className="col-span-12">
                   <h2 className="text-2xl">{title}</h2>
-                  <div><div dangerouslySetInnerHTML={{__html: processed}} /></div>
-                </div>
-                <div className="col-span-6 border border-gray-200">
-                  <div 
-                  className="cursor-pointer p-2 text-sm flex"
-                  onClick={ViewDetail(index)}>
-                  <div>View Detail</div>
-                  <div className="pt-1 px-2">
-                    <GrFormNextLink />
-                  </div>
-                </div>
-                </div>
-                <div className="col-span-6 border border-gray-200 p-2">Ticket</div>
+                  <div>{venue}</div>
               </div>
-          })
-        }
+              <div className="col-span-6 border border-gray-200">
+                  <div className="cursor-pointer p-2 text-sm flex"
+                    onClick={() => view(index)} >
+                  <div>View Detail</div>
+                  <div className="pt-1 px-2"><GrFormNextLink /></div>
+              </div>
+          </div>
+          <div className="col-span-6 border border-gray-200 p-2">Ticket</div>
       </div>
-
-
-    
-    
-    </div>
-  )
+    )
 }
 
 export default TourList02
