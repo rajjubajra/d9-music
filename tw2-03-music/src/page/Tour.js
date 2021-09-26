bc5dc2f45e9b9bdcc1eb3dedacbf8f4a6d7a160c
rajjubajra <rajjubajra@gmail.com>
2021-09-26 21:55:15 +0100

    tour page udpates

--- tw2-03-music/src/page/Tour.js
+++ tw2-03-music/src/page/Tour.js
@@ -7,13 +7,41 @@ import {actionTour} from '../redux/actions';
 function Tour() {
   const dispatch = useDispatch();
 
-  const tourdata = useSelector(state => state.reducerTour.tour_data);
+  const tourdata = useSelector(state => state.reducerTour.tour_data.data);
+  const fetched = useSelector(state => state.reducerTour.tour_fetched);
 
   console.log(tourdata);
 
   useEffect(()=>{
     dispatch(actionTour());
   },[dispatch])
+
+
+
+  console.log(tourdata, "fetched", fetched);
+
+  const [event, setEvent] = useState(0)
+  const [eventDate, setEventDate] = useState('');
+  const [title, setTitle] = useState('');
+  const [venue, setVenue] = useState('');
+  const [body, setBody] = useState('');
+
+  useEffect(()=>{
+    const {attributes:{field_event_date:date, title, 
+      field_event_location:{processed:venue}, 
+      field_event_details:{processed:body}}
+    } = tourdata;
+
+    setEventDate(date);
+    setTitle(title);
+    setVenue(venue);
+    setBody(body);
+  },[tourdata, event])
+
+  const view = (index) => ( setEvent(index) );
+
+  console.log(eventDate, title, venue, event, body);
+
 
   return (
     <div>
--- tw2-03/static/js/12.80f31444.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonptw2-03-music"]=this["webpackJsonptw2-03-music"]||[]).push([[12],{84:function(t,c,e){"use strict";e.r(c);var s=e(0),u=e(16),i=e(19),n=e(3);c.default=function(){var t=Object(u.b)(),c=Object(u.c)((function(t){return t.reducerTour.tour_data}));return console.log(c),Object(s.useEffect)((function(){t(Object(i.i)())}),[t]),Object(n.jsx)("div",{children:"this is test page"})}}}]);
-//# sourceMappingURL=12.80f31444.chunk.js.map
