8667b80580581ee74bd812c2fd1e483a830d12ee
rajjubajra <rajjubajra@gmail.com>
2021-09-26 21:45:41 +0100

    tour page udpates

--- tw2-03-music/src/page/Tour.js
+++ tw2-03-music/src/page/Tour.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {actionTour} from '../redux/actions';
+
+
 
 function Tour() {
+  const dispatch = useDispatch();
+
+  const tourdata = useSelector(state => state.reducderTour.tour_data);
+
+  console.log(tourdata);
+
+  useEffect(()=>{
+    dispatch(actionTour());
+  },[dispatch])
+
   return (
     <div>
       this is test page
