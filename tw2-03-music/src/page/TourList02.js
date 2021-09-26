28ee8b82bfb041f0cb1460d7e3fd3e4be4cb6fdb
rajjubajra <rajjubajra@gmail.com>
2021-09-26 15:59:38 +0100

    tour page udpates

--- tw2-03-music/src/page/TourList02.js
+++ tw2-03-music/src/page/TourList02.js
@@ -1,9 +1,7 @@
 import {GrFormNextLink} from 'react-icons/gr';
 
 
-
 function TourList02({date, title, venue, view, index}) {
-
 
   return (
       <div className="grid grid-cols-12 grid-flow-row gap-2 py-5
@@ -15,7 +13,7 @@ function TourList02({date, title, venue, view, index}) {
               </div>
               <div className="col-span-6 border border-gray-200">
                   <div className="cursor-pointer p-2 text-sm flex">
-                  <div>View Detail</div>
+                  <div>View Detail -></div>
                   <div className="pt-1 px-2"><GrFormNextLink /></div>
               </div>
           </div>
