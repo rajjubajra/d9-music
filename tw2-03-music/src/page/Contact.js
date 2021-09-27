bbc6e1ecc6f5b27b77cabb9bda34537a13c7a2b6
rajjubajra <rajjubajra@gmail.com>
2021-09-27 18:49:32 +0100

    tw2-03 news page udpates

--- tw2-03-music/src/page/Contact.js
+++ tw2-03-music/src/page/Contact.js
@@ -8,10 +8,6 @@ function Contact() {
 
   return (
     <div className="grid grid-cols-12 max-w-screen-xl m-auto p-10">
-      
-      <div className="col-span-12 md:col-span-4">
-        <ContactInfo />
-      </div>
 
       <div className="col-span-12 md:col-span-8 mt-10 md:mt-0">
         <ContactForm />
