cea5039fb7bccaafb2dc34ba16ca750af368179b
rajjubajra <rajjubajra@gmail.com>
2021-09-28 14:12:23 +0100

    tw2-03 news page udpates

--- tw2-03-music/src/page/Gallery/BlockAll.js
+++ tw2-03-music/src/page/Gallery/BlockAll.js
@@ -6,7 +6,7 @@ function BlockAll({image, title}) {
     <div className="col-span-8 lg:col-span-2 md:col-span-3">
           <div>
               <ImageOnload
-            cssClass="border border-gray-900"
+            cssClass="border border-gray-900 h-44 object-cover"
             src={image} />
           </div>
           <div className="text-xs uppercase">{title}</div>
